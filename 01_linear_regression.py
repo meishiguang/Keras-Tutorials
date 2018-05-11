ea5dbf87a4e5c7fd566b2e85ea00534d79b31b06
yangyongtai <yangyongtai@xdf.cn>
2018-05-12 05:53:38 +0800

    01_linear_regression fix with keras 2.0 version

--- 01_linear_regression.py
+++ 01_linear_regression.py
@@ -2,15 +2,34 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Generate dataset
 trX = np.linspace(-1, 1, 101)
-trY = 2 * trX + np.random.randn(*trX.shape) * 0.33 # create a y value which is approximately linear but with some random noise
+# create a y value which is approximately linear but with some random noise
+trY = 2 * trX + np.random.randn(*trX.shape) * 0.33
 
 # Linear regression model
 model = Sequential()
-model.add(Dense(output_dim=1, input_dim=1, init='normal', activation='linear'))
+model.add(Dense(activation='linear', kernel_initializer='normal', input_dim=1, output_dim=1))
 model.compile(optimizer=SGD(lr=0.01), loss='mean_squared_error', metrics=['accuracy'])
 
+# Print initial weights
+weights = model.layers[0].get_weights()
+w_init = weights[0][0][0]
+b_init = weights[1][0]
+
 # Train
-model.fit(trX, trY, nb_epoch=100, verbose=1)
+model.fit(trX, trY, epochs=100, verbose=1)
+
+#Print trained weights
+weights = model.layers[0].get_weights()
+w = weights[0][0][0]
+b = weights[1][0]
+print('Linear regression model is trained with weight w: %.2f, b: %.2f' %(w, b))
+
+plt.plot(trX, trY, label='data')
+plt.plot(trX, w_init*trX + b_init, label='init')
+plt.plot(trX, w*trX + b, label='predication')
+plt.legend()
+plt.show()
